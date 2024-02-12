93c460026363c547530afc28e6bbe981160ad5f6
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-02-12 22:07:08 +0530

    Update productList.js

--- src/component/productList.js
+++ src/component/productList.js
@@ -1,1 +1,11 @@
+import React from "react";
+import Product from "./product.js";
+
+export default function productList(props) {
+  //   console.log(props );
+
+  return props.productList.map((product, i) => {
+    return <Product product={product} key={i} />;
+  });
+}
 
